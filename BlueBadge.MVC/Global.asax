048985643c3a55678ed684f39123d6cedf6dbe13
aknight9255 <amanda.knight55@gmail.com>
2019-07-01 10:42:05 -0400

    Add project files.

---
+++ BlueBadge.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlueBadge.MVC.MvcApplication" Language="C#" %>
---
+++ BlueBadge.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlueBadge.MVC.Startup))]
+namespace BlueBadge.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
